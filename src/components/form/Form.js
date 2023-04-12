cb6cb04b404e5fbbd0bfabfe788eaf70eb53469b
Felipe Lima <102830741+LipePLima@users.noreply.github.com>
2023-04-12 12:05:33 -0300

    feat: Botão de formulário pronto e formulário pronto

--- src/components/form/Form.js
+++ src/components/form/Form.js
@@ -1,5 +1,6 @@
 import Field from "../FieldForm/FieldForm"
 import OptionsList from "../OptionsList/OptionsList";
+import Button from "../Button/Button";
 import "./Form.css";
 
 const Form = () => {
@@ -20,6 +21,9 @@ const Form = () => {
         <Field label="Cargo"  placeholder="Digite seu cargo" />
         <Field label="Imagem" placeholder="Informe o endereço da imagem" />
         <OptionsList label="Time"  itens={times}/>
+        <Button>
+          Criar Card
+        </Button>
       </form>
     </section>
   );
