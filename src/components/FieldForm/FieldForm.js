d88e4e875a91b66d57c7ff2855242b7437141627
Felipe Lima <102830741+LipePLima@users.noreply.github.com>
2023-04-15 15:47:27 -0300

    Capturando valor do input do FieldForm

--- src/components/FieldForm/FieldForm.js
+++ src/components/FieldForm/FieldForm.js
@@ -1,10 +1,19 @@
 import "./FieldForm.css";
 
 const FieldForm = (props) => {
+  let value = '';
+
+  const typed = (event) => {
+    value = event.target.value
+    console.log(value)
+
+  }
+
   return (
     <div className="container__field">
       <label className="field__label">{props.label}</label>
       <input
+        onChange={typed}
         required={props.required}
         className="field__input"
         type="text"
