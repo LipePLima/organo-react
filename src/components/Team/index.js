7080724d807233b54ef54aaf1171810071a2a22e
Felipe Lima <102830741+LipePLima@users.noreply.github.com>
2023-04-16 14:30:48 -0300

    Card feito

--- src/components/Team/index.js
+++ src/components/Team/index.js
@@ -1,12 +1,21 @@
-import "./Team.css"
+import Collaborator from "../collaborator";
+import "./Team.css";
 
 const Team = (props) => {
-    return (
-        <section className="container__team" style={{ backgroundColor: props.colorPrimary }}>
-            <h3 className="team__title" style={{ borderBottom: "4px solid" + props.colorSecundary }}>{props.title}</h3>
-            
-        </section>
-    )
+  return (
+    <section
+      className="container__team"
+      style={{ backgroundColor: props.colorPrimary }}
+    >
+      <h3
+        className="team__title"
+        style={{ borderBottom: "4px solid" + props.colorSecundary }}
+      >
+        {props.title}
+      </h3>
+      <Collaborator />
+    </section>
+  );
 };
 
 export default Team;
--- src/components/collaborator/index.js
+++ src/components/collaborator/index.js
@@ -2,7 +2,15 @@ import "./Collaborator.css"
 
 const Collaborator = () => {
     return (
-
+        <div className="container__card">
+            <div className="card__header">
+                <img src="https://github.com/lipeplima.png" alt="Imagem do Colaborador" />
+            </div>
+            <div className="card__footer">
+                <h4>Felipe Lima</h4>
+                <h5>Desenvolvedor Front-End</h5>
+            </div>
+        </div>
     )
 }
 
